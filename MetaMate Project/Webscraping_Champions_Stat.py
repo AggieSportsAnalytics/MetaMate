d1af45efc7127daf4ed633072202974a1e06dce8
QuocTrinh-coder <harrytrinh2004@gmail.com>
2023-11-22 03:46:27 -0800

    champions name, winrate, banrate and pick rate
    Webscrape Champion names, win rate, pick rate and ban rate from Blitz.gg and put it into a dataset using Pandas.

--- MetaMate Project/Webscraping_Champions_Stat.py
+++ MetaMate Project/Webscraping_Champions_Stat.py
@@ -1,40 +1,86 @@
-from bs4 import BeautifulSoup as soup
-import requests
-import re
-import time
+#
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
 
-# URL for the Blitz.gg champion statistics page
-url = "https://blitz.gg/lol/tierlist"
+# Initialize the Chrome driver
 
-# Send an HTTP GET request to the website
-response = requests.get(url)
 
-# Check if the request was successful (status code 200)
-Champion_names = []
-Corresponding_Win_rate = []
+# Set the URL
+url = "https://blitz.gg/lol/champions/overview"
+# Set the path to the uBlock Origin extension
+ublock_path = r"C:\Users\Harry Trinh\uBlock-Origin.crx"
 
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = soup(response.content, 'html.parser')
+# Configure Chrome options and add the uBlock Origin extension
+chrome_options = Options()
+chrome_options.add_extension(ublock_path)
 
-    table_container = soup.find('div',{'class':'card-frame card-contents'})
-    li_elements = table_container.find_all('li', class_ = "⚡464ba1e1")
-    for li_element in li_elements:
-        li_elements_2 = li_element.find_all('li', class_ = "⚡e1a31ee0")
-        for children in li_elements_2:
-            img_element = children.find('img', {"class": "⚡a243cb06 champion-image champion-img"})
-            win_rate = children.find("span", {"class" : "type-caption--semi"})
-            if img_element and win_rate:
-                alt_text = img_element.get('alt')
-                win_rate_text = win_rate.text 
-                Champion_names.append(alt_text)
-                Corresponding_Win_rate.append(win_rate_text)
-    
+# Initialize the Chrome driver with options
+driver = webdriver.Chrome(options=chrome_options)
+
+# Load the page
+driver.get(url)
+
+# Wait until the page is fully loaded (adjust the timeout as needed)
+try:
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'span.champion-name')),
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.cell.type-body2-form--bold'))
+    )
     print("Success")
+except Exception as e:
+    print(f"Error waiting for page to load: {e}")
+    driver.quit()
+    exit()
+
+# Get the initial page height
+previous_height = driver.execute_script('return document.body.scrollHeight')
+print("Success")
+
+# Scroll down until reaching the bottom of the page
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    print("Success1")
+    time.sleep(30)  # Adjust the sleep time based on your page loading speed
+
+    # Get the new page height
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    print("Success1")
+
+    # Break the loop if scrolling doesn't change the height (indicating the end of the page)
+    if new_height == previous_height:
+        print("Success2")
+        break
+
+    # Update the previous height for the next iteration
+    previous_height = new_height
+    print("Success1")
+
+
+# Print all span elements with class "champion-name"
+champion_spans = driver.find_elements(By.CSS_SELECTOR, 'span.champion-name')
+champion_names = [champion_span.text for champion_span in champion_spans]
+
+# Get all elements with class "cell type-body2-form--bold" containing percentage values using XPath
+percentage_elements = driver.find_elements(By.XPATH, '//div[contains(@class, "cell type-body2-form--bold") and contains(text(), "%")]')
+
+# Extract the text from the elements
+percentage_values = [element.text for element in percentage_elements]
+win_rate = percentage_values[0::3]
+ban_rate = percentage_values[1::3]
+pick_rate = percentage_values[2::3]
+
+
+# Quit the driver
+driver.quit()
 
 # Creating Data Frame
-data = {'Champion': Champion_names, 'WinRate(%)': Corresponding_Win_rate}
+data = {'Champion': champion_names, 'WinRate(%)': win_rate,'BanRate(%)': ban_rate, 'PickRate(%)': pick_rate, }
 
 # Create a DataFrame
 Champion_Stat = pd.DataFrame(data)
@@ -44,73 +90,16 @@ Champion_Stat['WinRate(%)'] = Champion_Stat['WinRate(%)'].str.replace('%', '')
 
 # Convert 'WinRate(%)' column to numeric (float)
 Champion_Stat['WinRate(%)'] = pd.to_numeric(Champion_Stat['WinRate(%)'])
+# Remove '%' character from the 'WinRate(%)' column
+Champion_Stat["BanRate(%)"] = Champion_Stat['BanRate(%)'].str.replace('%', '')
 
-print(Champion_Stat['WinRate(%)'])
+# Convert 'WinRate(%)' column to numeric (float)
+Champion_Stat['BanRate(%)'] = pd.to_numeric(Champion_Stat['BanRate(%)'])
+# Remove '%' character from the 'WinRate(%)' column
+Champion_Stat['PickRate(%)'] = Champion_Stat['PickRate(%)'].str.replace('%', '')
 
-
-#
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from bs4 import BeautifulSoup
-# import time
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+# Convert 'WinRate(%)' column to numeric (float)
+Champion_Stat['PickRate(%)'] = pd.to_numeric(Champion_Stat['PickRate(%)'])
 
 
-# # Set up a headless browser using Selenium
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
-
-# # Initialize the driver with options
-# driver = webdriver.Chrome(options=options)
-
-# url = "https://blitz.gg/lol/champions/overview"
-
-# # Load the page
-# driver.get(url)
-
-# # Get the initial page source
-# page_source = driver.page_source
-
-# # Parse the HTML content of the page
-# soup = BeautifulSoup(page_source, 'html.parser')
-
-# # Find all spans with class 'champion-name'
-# champion_spans = soup.find_all('span', {'class': 'champion-name'})
-
-# if champion_spans:
-#     # Loop through all found spans and print the champion names
-#     for champion_span in champion_spans:
-#         champion_name = champion_span.text
-#         print(f"Champion: {champion_name}")
-
-# # Scroll down the page to trigger additional content loading
-# for _ in range(3):  # You may need to adjust the number of scrolls based on the page structure
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    
-#     # Wait for the next link to be present before proceeding
-#     element_present = EC.presence_of_element_located((By.CSS_SELECTOR, '#main-content > div.⚡28fbadf2 > div.⚡de27659b.inner-wrapper-col > div > div > section > div > div > table > tbody > tr:nth-child(21) > td:nth-child(3) > a > span'))
-#     WebDriverWait(driver, 10).until(element_present)
-
-#     time.sleep(2)  # Add a delay to allow content to load
-
-# # Get the updated page source
-# page_source = driver.page_source
-
-# # Parse the updated HTML content
-# soup = BeautifulSoup(page_source, 'html.parser')
-
-# # Find all spans with class 'champion-name' again
-# champion_spans = soup.find_all('span', {'class': 'champion-name'})
-
-# if champion_spans:
-#     # Loop through all found spans and print the champion names
-#     for champion_span in champion_spans:
-#         champion_name = champion_span.text
-#         print(f"Champion: {champion_name}")
-# else:
-#     print("No additional champion names found on the page.")
-
-# # Close the browser
-# driver.quit()
+print(Champion_Stat)
